40b94948597bc28310bf70f5867f45a6ac44ca81
Marko Grujic <grujamar@gmail.com>
2019-05-23 12:25:47 +0200

    Working on Card-info and Diploma showing. Almost finished!

--- js/chooseUser.js
+++ js/chooseUser.js
@@ -19,60 +19,92 @@ $(document).ready(function () {
     var personId = GetURLParameter('personid');
     //console.log(personId);
     callAjaxUserInfo(personId);
-    
+
     function callAjaxUserInfo(personID) {
         $.ajax({
             url: "http://ucenickidomovi.pis.rs/VetWebService/api/person/" + personID,
             dataType: 'json',
             beforeSend: function () {
                 $('#user-profile').html('<img src="https://i.gifer.com/7YQl.gif">');
+                $('#card-profile').html('<img src="https://i.gifer.com/7YQl.gif">');
             }
         })
                 .done(function (data) {
                     console.log(data);
-                    
+                    /******************************PROFILE INFO***********************/
                     $('#user-img').empty();
                     $('#user-profile').empty();
 
                     $('#user-img').append('<figure><img src="https://api.adorable.io/avatars/150x150/abott@adorable.png" alt=""/></figure>');
-                    $('#user-profile').append('<div class="row"><div class="col-md-4"><label class="font-weight-bold">Ime</label></div><div class="col-md-8">' + data.Person.Name + '</div></div><hr>');
-                    $('#user-profile').append('<div class="row"><div class="col-md-4"><label class="font-weight-bold">Prezime</label></div><div class="col-md-8">' + data.Person.LastName + '</div></div><hr>');
-                    $('#user-profile').append('<div class="row"><div class="col-md-4"><label class="font-weight-bold">Titula</label></div><div class="col-md-8">' + data.Person.Title + '</div></div><hr>');
-                    $('#user-profile').append('<div class="row"><div class="col-md-4"><label class="font-weight-bold">Datum rodjenja</label></div><div class="col-md-8">' + (data.Person.DateOfBirth).substring(8, 10) + '-' + (data.Person.DateOfBirth).substring(5, 7) + '-' + (data.Person.DateOfBirth).substring(0, 4) + '</div></div><hr>');
-                    if (data.Person.Status === true)
+                    $('#user-profile').append('<div class="row"><div class="col-md-4"><label class="font-weight-bold">Ime</label></div><div class="col-md-8">' + data.Name + '</div></div><hr>');
+                    $('#user-profile').append('<div class="row"><div class="col-md-4"><label class="font-weight-bold">Prezime</label></div><div class="col-md-8">' + data.LastName + '</div></div><hr>');
+                    $('#user-profile').append('<div class="row"><div class="col-md-4"><label class="font-weight-bold">Titula</label></div><div class="col-md-8">' + data.Title + '</div></div><hr>');
+                    $('#user-profile').append('<div class="row"><div class="col-md-4"><label class="font-weight-bold">Datum rodjenja</label></div><div class="col-md-8">' + (data.DateOfBirth).substring(8, 10) + '-' + (data.DateOfBirth).substring(5, 7) + '-' + (data.DateOfBirth).substring(0, 4) + '</div></div><hr>');
+                    if (data.Status === true)
                     {
                         $('#user-profile').append('<div class="row"><div class="col-md-4"><label class="font-weight-bold">Status</label></div><div class="col-md-8 text-success">Aktivan</div></div><hr>');
                     } else {
                         $('#user-profile').append('<div class="row"><div class="col-md-4"><label class="font-weight-bold">Status</label></div><div class="col-md-8 text-danger">Neaktivan</div></div><hr>');
                     }
-
+                    /******************************CARDS INFO***********************/
                     $('#card-img').empty();
                     $('#card-profile').empty();
-                    //console.log("card profile konacno + " + data);
 
-                    $('#card-img').append('<span class="fa fa-id-card-o"></span>');
-                    $('#card-profile').append('<div class="row"><div class="col-md-4"><label class="font-weight-bold">Broj kartice</label></div><div class="col-md-8">' + data.CardNumber + '</div></div><hr>');
-                    $('#card-profile').append('<div class="row"><div class="col-md-4"><label class="font-weight-bold">Rok važenja kartice</label></div><div class="col-md-8">' + (data.CardValidity).substring(8, 10) + '-' + (data.CardValidity).substring(5, 7) + '-' + (data.CardValidity).substring(0, 4) + '</div></div><hr>');
-                    $('#card-profile').append('<div class="row"><div class="col-md-4"><label class="font-weight-bold">Poslednji put ažurirana</label></div><div class="col-md-8">' + (data.LastUpdateTime).substring(8, 10) + '-' + (data.LastUpdateTime).substring(5, 7) + '-' + (data.LastUpdateTime).substring(0, 4) + '</div></div><hr>');
-                    if (data.Status === true){
-                        $('#card-profile').append('<div class="row"><div class="col-md-4"><label class="font-weight-bold">Status</label></div><div class="col-md-8 text-success">Aktivna kartica</div></div><hr>');
+                    if (data.Cards.length < 1) {
+                        //console.log('Usao ovde ' + data.Cards.length);
+                        $('#card-profile').html('<p class="error"><strong>Ne postoji kartica za izabranu osobu.</strong></p>');
                     } else {
-                        $('#card-profile').append('<div class="row"><div class="col-md-4"><label class="font-weight-bold">Status</label></div><div class="col-md-8 text-danger">Neaktivna kartica</div></div><hr>');
+                        //console.log('Usao ovde ' + data.Cards.length);
+                        showCardInfoBasedOnLength(data.Cards[0].CardNumber, data.Cards[0].CardValidity, data.Cards[0].LastUpdateTime, data.Cards[0].Status);
                     }
+                    /******************************Diploma INFO***********************/
+                    var data1 = [];
+
+                    for (var x in data.Certificates) {
+                        data1.push([data.Certificates[x].Points,
+                            (data.Certificates[x].GraduateDate).substring(8, 10) + '-' + (data.Certificates[x].GraduateDate).substring(5, 7) + '-' + (data.Certificates[x].GraduateDate).substring(0, 4),
+                            data.Certificates[x].DepartmentId,
+                            data.Certificates[x].StudyLevelId,
+                            data.Certificates[x].TitleId,
+                            data.Certificates[x].ProfessionalOrientationId
+                        ]);
+                    }
+                    $('#exampleDiploma').DataTable({
+                        data: data1,
+                        scrollY: true,
+                        scrollX: false,
+                        scrollCollapse: true,
+                        paging: false,
+                        searching: false
+                    });
+
+
                 })
                 .fail(function (jqXHR, statusText) {
-                    if (jqXHR.statusText === "Not Found"){
-                        $('#user-profile').html('<p class="error"><strong>Greška!</strong> Ne postoji podaci za izabranu osobu.</p>');
-                    }else{
-                        $('#user-profile').text(jqXHR.status + '-' + jqXHR.statusText + '-' + statusText);
-                    }
-                    if (jqXHR.statusText === "Not Found"){
-                        $('#card-profile').html('<p class="error"><strong>Greška!</strong> Ne postoji kartica za izabranu osobu.</p>');
-                    }else{
-                        $('#card-profile').text(jqXHR.status + '-' + jqXHR.statusText + '-' + statusText);
-                    } 
+                    $('#user-profile').text(jqXHR.status + '-' + jqXHR.statusText + '-' + statusText);
+                    $('#card-profile').text(jqXHR.status + '-' + jqXHR.statusText + '-' + statusText);
+                    $('#diploma-info').text(jqXHR.status + '-' + jqXHR.statusText + '-' + statusText);
                 });
 
     }
 
+
+    function showCardInfoBasedOnLength(cardNumber, cardValidity, lasrUpdateTime, cardStatus) {
+        if (cardStatus === true) {
+            $('#card-img').append('<span class="fa fa-id-card-o"></span>');
+            $('#card-profile').append('<div class="row"><div class="col-md-4"><label class="font-weight-bold">Broj kartice</label></div><div class="col-md-8">' + cardNumber + '</div></div><hr>');
+            $('#card-profile').append('<div class="row"><div class="col-md-4"><label class="font-weight-bold">Rok važenja kartice</label></div><div class="col-md-8">' + (cardValidity).substring(8, 10) + '-' + (cardValidity).substring(5, 7) + '-' + (cardValidity).substring(0, 4) + '</div></div><hr>');
+            $('#card-profile').append('<div class="row"><div class="col-md-4"><label class="font-weight-bold">Poslednji put ažurirana</label></div><div class="col-md-8">' + (lasrUpdateTime).substring(8, 10) + '-' + (lasrUpdateTime).substring(5, 7) + '-' + (lasrUpdateTime).substring(0, 4) + '</div></div><hr>');
+            $('#card-profile').append('<div class="row"><div class="col-md-4"><label class="font-weight-bold">Status</label></div><div class="col-md-8 text-success">Aktivna kartica</div></div><hr>');
+        } else {
+            $('#card-profile').append('<div class="row"><div class="col-md-4"><label class="font-weight-bold">Status</label></div><div class="col-md-8 text-danger">Neaktivna kartica</div></div><hr>');
+        }
+    }
+
+
+
+    $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
+        $($.fn.dataTable.tables(true)).DataTable()
+                .columns.adjust();
+    });
 });
