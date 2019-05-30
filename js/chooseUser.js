fc5f5ca3de9cfdda6b7800213ed2949e7b3330f8
Marko Grujic <grujamar@gmail.com>
2019-05-30 10:26:43 +0200

    Try to protect js code, main_0 and chooseUser_0 disabled rightClick and ctrlU

--- js/chooseUser.js
+++ js/chooseUser.js
@@ -36,15 +36,15 @@ $(document).ready(function () {
                     $('#user-profile').empty();
 
                     $('#user-img').append('<figure><img src="https://api.adorable.io/avatars/150x150/abott@adorable.png" alt=""/></figure>');
-                    $('#user-profile').append('<div class="row"><div class="col-md-4"><label class="font-weight-bold">Ime</label></div><div class="col-md-8">' + data.Name + '</div></div><hr>');
-                    $('#user-profile').append('<div class="row"><div class="col-md-4"><label class="font-weight-bold">Prezime</label></div><div class="col-md-8">' + data.LastName + '</div></div><hr>');
-                    $('#user-profile').append('<div class="row"><div class="col-md-4"><label class="font-weight-bold">Titula</label></div><div class="col-md-8">' + data.Title + '</div></div><hr>');
-                    $('#user-profile').append('<div class="row"><div class="col-md-4"><label class="font-weight-bold">Datum rodjenja</label></div><div class="col-md-8">' + (data.DateOfBirth).substring(8, 10) + '-' + (data.DateOfBirth).substring(5, 7) + '-' + (data.DateOfBirth).substring(0, 4) + '</div></div><hr>');
+                    $('#user-profile').append('<div class="row"><div class="col-lg-4"><label class="font-weight-bold">Ime</label></div><div class="col-lg-8">' + data.Name + '</div></div><hr>');
+                    $('#user-profile').append('<div class="row"><div class="col-lg-4"><label class="font-weight-bold">Prezime</label></div><div class="col-lg-8">' + data.LastName + '</div></div><hr>');
+                    $('#user-profile').append('<div class="row"><div class="col-lg-4"><label class="font-weight-bold">Titula</label></div><div class="col-lg-8">' + data.Title + '</div></div><hr>');
+                    $('#user-profile').append('<div class="row"><div class="col-lg-4"><label class="font-weight-bold">Datum rodjenja</label></div><div class="col-lg-8">' + (data.DateOfBirth).substring(8, 10) + '-' + (data.DateOfBirth).substring(5, 7) + '-' + (data.DateOfBirth).substring(0, 4) + '</div></div><hr>');
                     if (data.Status === true)
                     {
-                        $('#user-profile').append('<div class="row"><div class="col-md-4"><label class="font-weight-bold">Status</label></div><div class="col-md-8 text-success">Aktivan</div></div><hr>');
+                        $('#user-profile').append('<div class="row"><div class="col-lg-4"><label class="font-weight-bold">Status</label></div><div class="col-lg-8 text-success">Aktivan</div></div><hr>');
                     } else {
-                        $('#user-profile').append('<div class="row"><div class="col-md-4"><label class="font-weight-bold">Status</label></div><div class="col-md-8 text-danger">Neaktivan</div></div><hr>');
+                        $('#user-profile').append('<div class="row"><div class="col-lg-4"><label class="font-weight-bold">Status</label></div><div class="col-lg-8 text-danger">Neaktivan</div></div><hr>');
                     }
                       
                     $('div.loading').remove(); 
@@ -100,12 +100,12 @@ $(document).ready(function () {
     function showCardInfoBasedOnLength(cardNumber, cardValidity, lasrUpdateTime, cardStatus) {
         if (cardStatus === true) {
             $('#card-img').append('<span class="fa fa-id-card-o"></span>');
-            $('#card-profile').append('<div class="row"><div class="col-md-4"><label class="font-weight-bold">Broj kartice</label></div><div class="col-md-8">' + cardNumber + '</div></div><hr>');
-            $('#card-profile').append('<div class="row"><div class="col-md-4"><label class="font-weight-bold">Rok važenja kartice</label></div><div class="col-md-8">' + (cardValidity).substring(8, 10) + '-' + (cardValidity).substring(5, 7) + '-' + (cardValidity).substring(0, 4) + '</div></div><hr>');
-            $('#card-profile').append('<div class="row"><div class="col-md-4"><label class="font-weight-bold">Poslednji put ažurirana</label></div><div class="col-md-8">' + (lasrUpdateTime).substring(8, 10) + '-' + (lasrUpdateTime).substring(5, 7) + '-' + (lasrUpdateTime).substring(0, 4) + '</div></div><hr>');
-            $('#card-profile').append('<div class="row"><div class="col-md-4"><label class="font-weight-bold">Status</label></div><div class="col-md-8 text-success">Aktivna kartica</div></div><hr>');
+            $('#card-profile').append('<div class="row"><div class="col-lg-4"><label class="font-weight-bold">Broj kartice</label></div><div class="col-lg-8">' + cardNumber + '</div></div><hr>');
+            $('#card-profile').append('<div class="row"><div class="col-lg-4"><label class="font-weight-bold">Rok važenja kartice</label></div><div class="col-lg-8">' + (cardValidity).substring(8, 10) + '-' + (cardValidity).substring(5, 7) + '-' + (cardValidity).substring(0, 4) + '</div></div><hr>');
+            $('#card-profile').append('<div class="row"><div class="col-lg-4"><label class="font-weight-bold">Poslednji put ažurirana</label></div><div class="col-lg-8">' + (lasrUpdateTime).substring(8, 10) + '-' + (lasrUpdateTime).substring(5, 7) + '-' + (lasrUpdateTime).substring(0, 4) + '</div></div><hr>');
+            $('#card-profile').append('<div class="row"><div class="col-lg-4"><label class="font-weight-bold">Status</label></div><div class="col-lg-8 text-success">Aktivna kartica</div></div><hr>');
         } else {
-            $('#card-profile').append('<div class="row"><div class="col-md-4"><label class="font-weight-bold">Status</label></div><div class="col-md-8 text-danger">Neaktivna kartica</div></div><hr>');
+            $('#card-profile').append('<div class="row"><div class="col-lg-4"><label class="font-weight-bold">Status</label></div><div class="col-lg-8 text-danger">Neaktivna kartica</div></div><hr>');
         }
     }
 
